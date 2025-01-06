0a84ef6ec10f2297481d8ee4d4d3a9f7d8049c83
Thanh Dat <caitlyn.ochamp@gmail.com>
2025-01-06 10:39:38 +0700

    fix: display

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ThemeProvider } from "@/ThemeProvider.jsx";
 import "@/assets/css/font.css";
-import "@/assets/css/reset.css";
+// import "@/assets/css/reset.css";
 import "@/assets/css/index.css";
 import App from './pages/Index';
 
